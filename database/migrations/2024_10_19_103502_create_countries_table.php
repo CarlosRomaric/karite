b1ceef6be949ccd4feaec3dbe40bbda74cc4d043
CarlosRomaric <carlos.nguetta@gmail.com>
2024-10-19 11:00:25 +0000

    ajout de la migration de la table  country

--- database/migrations/2024_10_19_103502_create_countries_table.php
+++ database/migrations/2024_10_19_103502_create_countries_table.php
@@ -12,7 +12,9 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('countries', function (Blueprint $table) {
-            $table->id();
+            $table->uuid();
+            $table->string('name')->nullable();
+            $table->string('postal_code')->nullable();
             $table->timestamps();
         });
     }
